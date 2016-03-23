4b6f082a14b59ba6fe7ca49a7eaa5908a9e97115
Raito Bezarius <masterancpp@gmail.com>
2016-03-23 23:51:56 +0100

    screens(welcome): add keypress

--- iron/screens/welcome.py
+++ iron/screens/welcome.py
@@ -1,7 +1,17 @@
 import urwid
+from .play import PlayScreen
 
 class WelcomeScreen(urwid.WidgetWrap):
 
     def __init__(self, game):
         super(WelcomeScreen, self).__init__(urwid.Filler(urwid.Text("Welcome on Iron Strategy!"), 'top'))
         self.game = game # now we can push PlayScreen if clicked on it
+
+    def keypress(self, size, key):
+        if key == 'p':
+            self.game.game_state.push_screen(PlayScreen(self.game))
+
+    def selectable(self):
+        return True
+
+
