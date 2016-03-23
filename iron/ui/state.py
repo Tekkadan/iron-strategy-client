1dc097ba75c722b6e3b1ffa1491b37113737c790
Raito Bezarius <masterancpp@gmail.com>
2016-03-23 23:29:33 +0100

    iron(game-state): add stack debug

--- iron/ui/state.py
+++ iron/ui/state.py
@@ -5,12 +5,20 @@ class StateManager(urwid.WidgetWrap):
         super(StateManager, self).__init__(screen)
         self.state = [screen]
 
+        self._show_stack()
+
+    def _show_stack(self):
+        print ('Stack screen: {} (last is the first to be depopped)'\
+               .format(' --> '.join(map(str, self.state))))
+
     def push_screen(self, screen):
         self.state.append(screen)
+        self._show_stack()
 
     def pop_screen(self):
         self.state.pop()
         self._w = self.peek()
+        self._show_stack()
 
     def peek(self):
         return self.state[0]
