5aa8b74f2daa417fe572c8f1b79092b105a25847
Raito Bezarius <masterancpp@gmail.com>
2016-03-23 23:23:54 +0100

    iron(game-state): add stacking game state

--- iron/ui/state.py
+++ iron/ui/state.py
@@ -1,8 +1,16 @@
 import urwid
 
 class StateManager(urwid.WidgetWrap):
-    def __init__(self, state):
-        self.state = state
+    def __init__(self, game, screen):
+        super(StateManager, self).__init__(screen)
+        self.state = [screen]
 
-    def login():
-        pass # later
+    def push_screen(self, screen):
+        self.state.append(screen)
+
+    def pop_screen(self):
+        self.state.pop()
+        self._w = self.peek()
+
+    def peek(self):
+        return self.state[0]
