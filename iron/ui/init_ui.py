21e8cc9f3d1ac94ee9805d7bffcf97a71d3be165
Raito Bezarius <masterancpp@gmail.com>
2016-03-23 23:23:28 +0100

    iron(initialization): add Application

--- iron/ui/init_ui.py
+++ iron/ui/init_ui.py
@@ -1,15 +1,24 @@
 import urwid
+import asyncio
 from iron.ui.state import StateManager
+from iron.screens.welcome import WelcomeScreen
 
 basepalette = [
     ('banner', 'black', 'light gray', 'bold'),
     ('streak', 'black,' 'dark red'),
     ('bg', 'black', 'dark blue'),]
 
+class Application:
+
+    def __init__(self):
+        self.game_state = StateManager(self, WelcomeScreen())
+        self.evl = urwid.AsyncioEventLoop(loop=asyncio.get_event_loop())
+        self.loop = urwid.MainLoop(self.game_state, event_loop=self.evl)
+        print (self.game_state)
+
+    def start(self):
+        self.loop.run()
+        return self
+
 def ui_init():
-    placeholder = urwid.SolidFill()
-    gameLoop = urwid.AsyncioEventLoop()
-    gameLoop.widget = StateManager('initial')
-
-    gameLoop.run()
-
+    return Application().start()
