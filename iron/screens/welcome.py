49bc7156422b461b99a944360c039fc79e592044
Abridged Kirigami <abridgedkirigami@gmail.com>
2016-03-24 17:02:57 -0400

    Added build model from play.py to the welcome screen.

--- iron/screens/welcome.py
+++ iron/screens/welcome.py
@@ -4,14 +4,20 @@ from .play import PlayScreen
 class WelcomeScreen(urwid.WidgetWrap):
 
     def __init__(self, game):
-        super(WelcomeScreen, self).__init__(urwid.Filler(urwid.Text("Welcome on Iron Strategy!"), 'top'))
+        super(WelcomeScreen, self).__init__(self.build())
         self.game = game # now we can push PlayScreen if clicked on it
 
     def keypress(self, size, key):
         if key == 'p':
             self.game.game_state.push_screen(PlayScreen(self.game))
+        elif key == 'q':
+            self.game.exitMainLoop()
 
     def selectable(self):
         return True
 
-
+    def build(self):
+        return urwid.Filler(
+            urwid.Text('Welcome to Iron Strategy ! Press P to begin.', align='center'),
+            'middle'
+        )
