a9f4805a6d8c3009e9c828a98ccd7ae48a576820
Abridged Kirigami <abridgedkirigami@gmail.com>
2016-03-27 17:06:50 -0400

    colors !

--- iron/screens/welcome.py
+++ iron/screens/welcome.py
@@ -11,13 +11,12 @@ class WelcomeScreen(urwid.WidgetWrap):
         if key == 'p':
             self.game.game_state.push_screen(PlayScreen(self.game))
         elif key == 'q':
-            self.game.exitMainLoop()
+            self.game.ExitMainLoop()
 
     def selectable(self):
         return True
 
     def build(self):
-        return urwid.Filler(
-            urwid.Text('Welcome to Iron Strategy ! Press P to begin.', align='center'),
-            'middle'
-        )
+        return urwid.AttrMap(urwid.Filler(
+                urwid.AttrMap(urwid.Text('Iron Strategy', align='center'), 'banner')
+                ), 'bg')
--- iron/ui/init_ui.py
+++ iron/ui/init_ui.py
@@ -4,8 +4,8 @@ from iron.ui.state import StateManager
 from iron.screens.welcome import WelcomeScreen
 
 basepalette = [
-    ('banner', 'black', 'light gray', 'bold'),
-    ('streak', 'black,' 'dark red'),
+    ('banner', 'black', 'light gray'),
+    ('streak', 'black', 'dark green'),
     ('bg', 'black', 'dark blue'),]
 
 class Application:
@@ -13,7 +13,7 @@ class Application:
     def __init__(self):
         self.game_state = StateManager(self, WelcomeScreen(self))
         self.evl = urwid.AsyncioEventLoop(loop=asyncio.get_event_loop())
-        self.loop = urwid.MainLoop(self.game_state, event_loop=self.evl)
+        self.loop = urwid.MainLoop(self.game_state, palette=basepalette, event_loop=self.evl)
 
     def start(self):
         self.loop.run()
